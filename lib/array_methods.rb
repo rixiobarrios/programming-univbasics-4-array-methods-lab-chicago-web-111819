f16f907d8ca680cdd0b9364abc8ea14ef40d8730
Rixio Barrios <rixiobarrios.consulado@gmail.com>
2019-10-18 19:00:00 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,7 +1,9 @@
 def using_include(array, element)
   array = ["wow", "I", "am", "really", "learning", "arrays!"]
-  array.include?("wow")
-  array.include?("Tardis")
+   if array.include?("wow")
+     return true
+   if array.include?("Tardis")
+     return false
 end
 
 def using_sort(array)
